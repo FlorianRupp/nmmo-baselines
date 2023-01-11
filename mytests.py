20967ffa3aa2e9bfb2aa1a08d6611803c5892542
florian.rupp95@gmail.com <florian.rupp95@gmail.com>
2023-01-11 16:42:11 +0100

    Dummy map gen with stone border.

--- mytests.py
+++ mytests.py
@@ -1,8 +1,9 @@
 '''Documented at neuralmmo.github.io'''
-
+import random
 from pdb import set_trace as T
 
 import nmmo
+from nmmo import Terrain
 import numpy as np
 from random import shuffle
 
@@ -45,15 +46,33 @@ class CustomMapGenerator(nmmo.MapGenerator):
         '''Override the default per-map generation method'''
         size = self.config.MAP_SIZE
 
-        print("SIZE", size)
-
         # Create fractal and material placeholders
         fractal = np.zeros((size, size))  # Unused in demo
-        matl = np.array([nmmo.Terrain.WATER, nmmo.Terrain.FOREST] * int(size / 2) * size).reshape(size, size)
-
+        matl = np.array([nmmo.Terrain.WATER, nmmo.Terrain.TREE] * int(size / 2) * size).reshape(size, size)
         # Return signature includes fractal and material
         # Pass a zero array if fractal is not relevant
         print(matl.shape)
+        return fractal, matl
+
+
+class DummyMapGenerator(nmmo.MapGenerator):
+    # only works with size 6
+    def generate_map(self, idx):
+        size = self.config.MAP_SIZE
+        center_size = self.config.MAP_CENTER
+
+        fractal = np.zeros((size, size))
+
+        terrains = {Terrain.STONE: 0.2, Terrain.GRASS: 0.4, Terrain.FOREST: 0.1, Terrain.TREE: 0.1, Terrain.ORE: 0.025,
+                    Terrain.CRYSTAL: 0.025, Terrain.WATER: 0.15}
+
+        matl = np.random.choice(list(terrains.keys()), size=(center_size, center_size), p=list(terrains.values())).astype(np.uint8)
+        matl[0] = [Terrain.STONE] * center_size
+        matl[-1] = [Terrain.STONE] * center_size
+        matl[:, 0] = [Terrain.STONE] * center_size
+        matl[:, -1] = [Terrain.STONE] * center_size
+
+        matl = np.pad(matl, pad_width=self.config.MAP_BORDER)
         return fractal, matl
 
 
@@ -119,15 +138,15 @@ class Config(nmmo.config.Medium, nmmo.config.AllGameSystems):
 
     NPC_N = 0
 
-    MAP_N_TILE = 1
+    # MAP_N_TILE = 1
     MAP_GENERATE_PREVIEWS = True
-    MAP_PREVIEW_DOWNSCALE = 5
-    # MAP_SIZE = 16
-    MAP_CENTER = 8
-    # MAP_BORDER = 4
-    MAP_GENERATOR = CustomMapGenerator
+    MAP_PREVIEW_DOWNSCALE = 1
+    MAP_CENTER = 10
+    # MAP_BORDER = 0
+    MAP_GENERATOR = DummyMapGenerator
+    LOG_VERBOSE = False
 
 
 if __name__ == '__main__':
     simulate(nmmo.Env, Config, render=True)
-    # gen_map(nmmo.Env, Config)
+    gen_map(nmmo.Env, Config)
