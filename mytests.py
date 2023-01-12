22a7e3b1a4f10e815505bdf79ad3a88e8ef0f6c8
Florian Rupp <rupp@hdm-stuttgart.de>
2023-01-12 17:45:02 +0100

    Working on spawning.

--- mytests.py
+++ mytests.py
@@ -9,6 +9,9 @@ from random import shuffle
 
 # Scripted models included with the baselines repository
 from scripted import baselines
+from nmmo.lib.spawn import spawn_concurrent, spawn_continuous
+import sys
+import nmmo.core.realm
 
 
 def simulate(env, config, render=False, horizon=float('inf')):
@@ -19,7 +22,7 @@ def simulate(env, config, render=False, horizon=float('inf')):
     env.reset()
 
     t = 0
-    while True:
+    while env.num_agents != 0:
         if render:
             env.render()
 
@@ -31,8 +34,10 @@ def simulate(env, config, render=False, horizon=float('inf')):
         if t >= horizon:
             break
 
+    print("All agents died, stopping now")
+    env.close()
     # Called at the end of simulation to obtain logs
-    return env.terminal()
+    # return env.terminal()
 
 
 def gen_map(env, config, render=False, horizon=float('inf')):
@@ -48,7 +53,8 @@ class CustomMapGenerator(nmmo.MapGenerator):
 
         # Create fractal and material placeholders
         fractal = np.zeros((size, size))  # Unused in demo
-        matl = np.array([nmmo.Terrain.WATER, nmmo.Terrain.TREE] * int(size / 2) * size).reshape(size, size)
+        # matl = np.array([nmmo.Terrain.WATER, nmmo.Terrain.TREE] * int(size / 2) * size).reshape(size, size)
+        matl = np.array([nmmo.Terrain.GRASS] * size * size).reshape(size, size)
         # Return signature includes fractal and material
         # Pass a zero array if fractal is not relevant
         print(matl.shape)
@@ -66,7 +72,8 @@ class DummyMapGenerator(nmmo.MapGenerator):
         terrains = {Terrain.STONE: 0.2, Terrain.GRASS: 0.4, Terrain.FOREST: 0.1, Terrain.TREE: 0.1, Terrain.ORE: 0.025,
                     Terrain.CRYSTAL: 0.025, Terrain.WATER: 0.15}
 
-        matl = np.random.choice(list(terrains.keys()), size=(center_size, center_size), p=list(terrains.values())).astype(np.uint8)
+        matl = np.random.choice(list(terrains.keys()), size=(center_size, center_size),
+                                p=list(terrains.values())).astype(np.uint8)
         matl[0] = [Terrain.STONE] * center_size
         matl[-1] = [Terrain.STONE] * center_size
         matl[:, 0] = [Terrain.STONE] * center_size
@@ -110,7 +117,14 @@ class CustomMapGenerator2(nmmo.MapGenerator):
         return fractal, matl
 
 
-class Config(nmmo.config.Medium, nmmo.config.AllGameSystems):
+# class MyPlayerLoader(nmmo.lib.spawn):
+#     pass
+
+def spawn(config):
+    return [(16, 16), (16, 18)]
+
+
+class Config(nmmo.config.Small, nmmo.config.AllGameSystems):
     '''Config objects subclass a nmmo.config.{Small, Medium, Large} template
 
     Can also specify config game systems to enable various features'''
@@ -145,8 +159,25 @@ class Config(nmmo.config.Medium, nmmo.config.AllGameSystems):
     # MAP_BORDER = 0
     MAP_GENERATOR = DummyMapGenerator
     LOG_VERBOSE = False
+    LOG_EVENTS = False
+
+    PROGRESSION_SYSTEM_ENABLED = False
+
+    PLAYER_SPAWN_FUNCTION = spawn
+    # PLAYER_SPAWN_FUNCTION = spawn_concurrent
+    # TODO big pormblem is how to decide where agents spawn in a cumstom function
+    # how to frame it competitive?
+
+
+def printConfig(config):
+    for attr in dir(config):
+        if not attr.startswith('__'):
+            print('{}: {}'.format(attr, getattr(config, attr)))
 
 
 if __name__ == '__main__':
+    # printConfig(Config())
+    # print("myspawn", spawn(Config()))
+    # print(spawn_concurrent(Config()))
     simulate(nmmo.Env, Config, render=True)
-    gen_map(nmmo.Env, Config)
+    # gen_map(nmmo.Env, Config)
